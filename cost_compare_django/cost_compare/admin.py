a7a22c54e8d1de24e411b6eb9ab7065817143dda
LehmanBrother <chrislehman1991@gmail.com>
2018-11-28 16:17:45 -0600

    Additional models and migrations

--- cost_compare_django/cost_compare/admin.py
+++ cost_compare_django/cost_compare/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Agency, Federal_Account, Consumer_Reference, Agency_Raw, Federal_Account_Raw
 
 # Register your models here.
+admin.site.register(Agency)
+admin.site.register(Federal_Account)
+admin.site.register(Consumer_Reference)
+admin.site.register(Agency_Raw)
+admin.site.register(Federal_Account_Raw)
--- cost_compare_django/cost_compare/models.py
+++ cost_compare_django/cost_compare/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Agency(models.Model):
 	name = models.CharField(max_length=128)
 	agency_id = models.IntegerField(default=0)
-	budget_authority_amount = models.Float()
+	budget_authority_amount = models.FloatField()
 	budget_total = models.FloatField()
 	budget_pct = models.FloatField()
 
@@ -14,9 +14,9 @@ class Agency(models.Model):
 
 class Federal_Account(models.Model):
 	name = models.CharField(max_length=128)
-	id = models.IntegerField(default=0)
+	account_id = models.IntegerField(default=0)
 	amount = models.FloatField()
-	parent = models.ForeignKey(Agency, related_name="federal_accounts")
+	parent = models.ForeignKey('Agency', on_delete=models.CASCADE, related_name="federal_accounts")
 
 	def __str__(self):
 		return self.name
@@ -24,7 +24,33 @@ class Federal_Account(models.Model):
 class Consumer_Reference(models.Model):
 	name = models.CharField(max_length=128)
 	price = models.FloatField()
-	user = models.ForeignKey(User, related_name="consumer_references")
+	user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="consumer_references")
+
+	def __str__(self):
+		return self.name
+
+class Agency_Raw(models.Model):
+	percentage_of_total_budget_authority = models.FloatField()
+	budget_authority_amount = models.FloatField()
+	obligated_amount = models.FloatField()
+	active_fq = models.IntegerField(default=0)
+	active_fy = models.IntegerField(default=0)
+	outlay_amount = models.FloatField()
+	abbreviation = models.CharField(max_length=32)
+	agency_id = models.IntegerField(default=0)
+	agency_name = models.CharField(max_length=128)
+	current_total_budget_authority_amount = models.FloatField()
+
+	def __str__(self):
+		return self.name
+
+class Federal_Account_Raw(models.Model):
+	name = models.CharField(max_length=256)
+	account_id = models.IntegerField(default=0)
+	account_number = models.CharField(max_length=16)
+	code = models.IntegerField(default=0)
+	amount = models.FloatField()
+	category = models.CharField(max_length=32)
 
 	def __str__(self):
 		return self.name
